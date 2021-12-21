d46ebb90c7a28f2a19be7008831429d6020f962d
fnramirez93 <fnramirez93@gmail.com>
2021-12-20 22:58:25 -0300

    cambios2

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//comentario de uan sola sola linea//
+/*comentarios con mas de una linea*/
+alert("Calculador de prestamos | Eunoia |");
+let nombreIngresado = prompt("Ingresa Nombre y Apellido");
+let salida = "Bienvenido " + "| " + nombreIngresado + " |";
+alert(salida);
+let a = prompt("Ingrsa el Monto que necesitas");
+let b = prompt("Ingresa el Número de Cuotas");
+let r = 4/3;
+let c = a * r;
+let d = c / b;
+let mensaje = "Su prestamo se le dara a pagar en " + b + " cuotas de $" + d;  
+alert(mensaje); 
